23ab461e8a87ee63d76e83d93a82f28fe6d4d156
silofy <silofy@outlook.com>
2020-03-21 17:50:03 -0300

    mega bump

--- scripts/build.js
+++ scripts/build.js
@@ -1,24 +1,16 @@
 const fs = require('fs');
 const path = require('path');
-const generate = require('./generate');
-
 const THEME_DIR = path.join(__dirname, '..', 'theme');
-
 if (!fs.existsSync(THEME_DIR)) {
     fs.mkdirSync(THEME_DIR);
 }
-
-module.exports = async () => {
-    const { base, soft } = await generate();
-
-    return Promise.all([
-        fs.promises.writeFile(
-            path.join(THEME_DIR, 'HackTheBox.json'),
-            JSON.stringify(base, null, 4)
-        ),
-    ]);
-};
-
-if (require.main === module) {
-    module.exports();
+async function generate() {
+    const jsonFile = fs.readFileSync(
+        path.join(__dirname, '..', 'themes', 'HackTheBox.json'),
+        'utf-8');
+    fs.writeFileSync(
+        path.join(THEME_DIR, 'HackTheBox.json'),
+        jsonFile
+    )
 }
+generate()
